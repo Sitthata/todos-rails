0fdb8dbdc4b4ee5550f375a654890a80f75ac192
Sitthata <aprils96950@gmail.com>
2024-11-02 20:08:16 +0700

    Refactor Assignee and Todo views for improved styling and structure

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   resources :assignees do
     resources :todos, only: [ :index ]
   end
+
+  resources :todos
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
