01dde7a6ab7dfb470fec190bb8833d74a2358257
Sitthata <aprils96950@gmail.com>
2024-11-02 19:20:45 +0700

    add assignee models

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,9 @@
 class Todo < ApplicationRecord
+  belongs_to :assignee
+
+  validates :title, presence: true
+  validates :assignee, presence: true
+
   def completed?
     completed == 1
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_11_02_042048) do
+ActiveRecord::Schema[7.2].define(version: 2024_11_02_121758) do
+  create_table "assignees", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "todos", force: :cascade do |t|
     t.text "title"
     t.integer "completed"
